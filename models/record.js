42a092a0893ebb2575d8613269eaecf2184a6e46
linway035 <wayneck0138@gmail.com>
2022-11-07 18:35:39 +0800

    feat: add 3 models

---
+++ models/record.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const recordSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  userId: {
+    //Mongoose 提供的 Populate 功能，
+    type: Schema.Types.ObjectId, //定義 userId 這個項目是一個 ObjectId，也就是它會連向另一個資料物件
+    ref: "User", //定義參考對象是 User model
+    index: true, //把 userId 設定成「索引」，使用索引來查詢資料能夠增加讀取效能
+    required: true,
+  },
+  categoryId: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+    index: true,
+    required: true,
+  },
+});
+module.exports = mongoose.model("Record", recordSchema);
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true, //之後測試是否能用同名 以及空白是否trim
+    // trim: true, //空白是否trim有效
+  },
+  email: {
+    type: String, //沒有email這type
+    // required: true, //若不能同名就保留
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model("User", userSchema);
