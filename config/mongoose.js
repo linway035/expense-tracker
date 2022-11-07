d0b66e464e30fcc0dfd65e0bc4aa8d8f99078254
linway035 <wayneck0138@gmail.com>
2022-11-07 17:30:17 +0800

    Initialize Project

---
+++ config/mongoose.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const MONGODB_URI = process.env.MONGODB_URI;
+// 設定連線到 mongoDB
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+// 取得資料庫連線狀態
+const db = mongoose.connection;
+// 連線異常
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+// 連線成功
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
+
+module.exports = db;
