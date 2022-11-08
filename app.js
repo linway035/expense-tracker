487aa504234fe6a4bf3a5282886fb20711cd4d75
linway035 <wayneck0138@gmail.com>
2022-11-08 12:52:19 +0800

    feat: add handlebars, including main.hbs

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// 載入 express 並建構應用程式伺服器
-const express = require("express");
+const express = require("express"); // 載入 express 並建構應用程式伺服器
+const exphbs = require("express-handlebars");
 
 //dotenv設置
 if (process.env.NODE_ENV !== "production") {
@@ -9,6 +9,13 @@ if (process.env.NODE_ENV !== "production") {
 const routes = require("./routes");
 const app = express();
 const port = process.env.PORT;
+
+// setting static files靜態檔案
+app.use(express.static("public"));
+
+// setting template engine
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
 
 // 將 request 導入路由器
 app.use(routes);
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // 定義首頁路由
 router.get("/", (req, res) => {
-  res.send("hello world");
+  res.render("index");
 });
 // 匯出路由模組
 module.exports = router;
