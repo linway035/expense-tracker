33c9d072fa310a5c6f24c537afb0bf7fe1b5fac6
linway035 <wayneck0138@gmail.com>
2022-11-11 10:59:52 +0800

    style: JavaScript Standard Style

--- config/passport.js
+++ config/passport.js
@@ -1,17 +1,17 @@
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy; // passport-local的Strategy物件
-const FacebookStrategy = require("passport-facebook").Strategy;
-const User = require("../models/user");
-const bcrypt = require("bcryptjs");
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy // passport-local的Strategy物件
+const FacebookStrategy = require('passport-facebook').Strategy
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 module.exports = (app) => {
   // 初始化 Passport 模組
-  app.use(passport.initialize());
-  app.use(passport.session());
+  app.use(passport.initialize())
+  app.use(passport.session())
   // 設定本地登入策略
   passport.use(
     new LocalStrategy(
-      { usernameField: "email", passReqToCallback: true },
+      { usernameField: 'email', passReqToCallback: true },
       (req, email, password, done) => {
         // 把驗證項目從預設的 username 改成 email。
         // 欄位名稱email是字串所以要加引號
@@ -23,8 +23,8 @@ module.exports = (app) => {
               return done(
                 null,
                 false,
-                req.flash("warning_msg", "That email is not registered!")
-              ); // If the credential does not belong to a known user, or is not valid, the verify function calls the callback with false to indicate an authentication failure。 找不到使用者輸入的 username，用戶物件為 false
+                req.flash('warning_msg', 'That email is not registered!')
+              ) // If the credential does not belong to a known user, or is not valid, the verify function calls the callback with false to indicate an authentication failure。 找不到使用者輸入的 username，用戶物件為 false
             }
             // 第一個參數是使用者的輸入值，而第二個參數是資料庫裡的雜湊值，bcrypt 會幫我們做比對，並回傳布林值 (我設為isMatch)
             return bcrypt.compare(password, user.password).then((isMatch) => {
@@ -33,17 +33,17 @@ module.exports = (app) => {
                 return done(
                   null,
                   false,
-                  req.flash("warning_msg", "Email or Password incorrect.")
-                ); // 找到使用者，但發現密碼錯誤，用戶物件為 false
+                  req.flash('warning_msg', 'Email or Password incorrect.')
+                ) // 找到使用者，但發現密碼錯誤，用戶物件為 false
               }
-              return done(null, user); // success
-            });
+              return done(null, user) // success
+            })
           })
           // If an error occurs, such as the database not being available, the callback is called with an error
-          .catch((err) => done(err, false)); // 其實 done(err) 就夠了
+          .catch((err) => done(err, false)) // 其實 done(err) 就夠了
       }
     )
-  );
+  )
 
   // 設定facebook策略
   passport.use(
@@ -52,13 +52,13 @@ module.exports = (app) => {
         clientID: process.env.FACEBOOK_ID,
         clientSecret: process.env.FACEBOOK_SECRET,
         callbackURL: process.env.FACEBOOK_CALLBACK,
-        profileFields: ["email", "displayName"],
+        profileFields: ['email', 'displayName']
       },
       (accessToken, refreshToken, profile, done) => {
-        const { name, email } = profile._json;
+        const { name, email } = profile._json
         User.findOne({ email }).then((user) => {
-          if (user) return done(null, user);
-          const randomPassword = Math.random().toString(36).slice(-8);
+          if (user) return done(null, user)
+          const randomPassword = Math.random().toString(36).slice(-8)
           bcrypt
             .genSalt(10)
             .then((salt) => bcrypt.hash(randomPassword, salt))
@@ -66,15 +66,15 @@ module.exports = (app) => {
               User.create({
                 name,
                 email,
-                password: bcrypt.hash,
+                password: bcrypt.hash
               })
             )
             .then((user) => done(null, user))
-            .catch((err) => done(err, false));
-        });
+            .catch((err) => done(err, false))
+        })
       }
     )
-  );
+  )
   // 設定序列化與反序列化
   passport.serializeUser((user, done) => {
     // console.log(user);
@@ -86,12 +86,12 @@ module.exports = (app) => {
     //   createdAt: 2022-11-01T08:29:12.292Z,
     //   __v: 0
     // }
-    done(null, user.id); // mongoose幫忙做出的_id，這裡用別名(不用底線)id也可
-  });
+    done(null, user.id) // mongoose幫忙做出的_id，這裡用別名(不用底線)id也可
+  })
   passport.deserializeUser((id, done) => {
     User.findById(id)
       .lean() // 把資料庫物件轉換成 JavaScript 原生物件
       .then((user) => done(null, user))
-      .catch((err) => done(err, null)); // 其實Passport看到第一個參數有 err 就不會處理後面的參數了，放一個 null 只是語意表達用
-  });
-};
+      .catch((err) => done(err, null)) // 其實Passport看到第一個參數有 err 就不會處理後面的參數了，放一個 null 只是語意表達用
+  })
+}
--- models/record.js
+++ models/record.js
@@ -1,30 +1,30 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 const recordSchema = new Schema({
   name: {
     type: String,
-    required: true,
+    required: true
   },
   date: {
     type: Date,
-    required: true,
+    required: true
   },
   amount: {
     type: Number,
-    required: true,
+    required: true
   },
   userId: {
     // Mongoose 提供的 Populate 功能，
     type: Schema.Types.ObjectId, // 定義 userId 這個項目是一個 ObjectId，也就是它會連向另一個資料物件
-    ref: "User", // 定義參考對象是 User model
+    ref: 'User', // 定義參考對象是 User model
     index: true, // 把 userId 設定成「索引」，使用索引來查詢資料能夠增加讀取效能
-    required: true,
+    required: true
   },
   categoryId: {
     type: Number, // 可用number,string,或Schema.Types.ObjectId，各有相應的作法
-    ref: "Category",
+    ref: 'Category',
     index: true,
-    required: true,
-  },
-});
-module.exports = mongoose.model("Record", recordSchema);
+    required: true
+  }
+})
+module.exports = mongoose.model('Record', recordSchema)
