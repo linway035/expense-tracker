d04c5e4a72e046c41d916feb606797703f8c97d6
linway035 <wayneck0138@gmail.com>
2022-11-11 09:23:23 +0800

    feat: add passport

--- app.js
+++ app.js
@@ -1,21 +1,98 @@
-const express = require("express"); // 載入 express 並建構應用程式伺服器
+const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
+const flash = require("connect-flash");
 
-//dotenv設置
+//dotenv把東西放入process.env
+// console.log("before", process.env);
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+// console.log("after", process.env);
 
-const routes = require("./routes");
+const routes = require("./routes"); //預設會去找index.js，故省略(routes/index.js)
+const usePassport = require("./config/passport"); //檔名，而非module名(有這東西嗎?)
+require("./config/mongoose"); //對 app.js 而言，Mongoose 連線設定只需要「被執行」，不需要接到任何回傳參數繼續利用，所以這裡不需要再設定變數。
+
+//要注意 程式是由上而下讀取，所以port用了環境變數 必須放在dotenv那段的下面才讀得到
 const app = express();
 const port = process.env.PORT;
 
 // setting static files靜態檔案
 app.use(express.static("public"));
 
-// setting template engine
-app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
-app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+    helpers: {
+      iconImage: function (number) {
+        const category_image = {
+          家居物業: '<i class="fa-solid fa-house"></i>',
+          交通出行: '<i class="fa-solid fa-van-shuttle"></i>',
+          休閒娛樂: '<i class="fa-solid fa-face-grin-beam"></i>',
+          餐飲食品: '<i class="fa-solid fa-utensils"></i>',
+          其他: '<i class="fa-solid fa-pen"></i>',
+        };
+        const category_number = {
+          0: "類別",
+          1: "家居物業",
+          2: "交通出行",
+          3: "休閒娛樂",
+          4: "餐飲食品",
+          5: "其他",
+        };
+        const item = category_number[number];
+        return category_image[item];
+      },
+      dateFormat: function (dateFromDB) {
+        return new Date(dateFromDB).toLocaleDateString();
+      },
+    },
+  })
+); //建立名為hbs的樣板引擎
+app.set("view engine", "hbs"); //啟用樣板引擎
+
+//設定 express-session，需要放在較前面
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET, //這個參數是 session 用來驗證 session id 的字串。這組字串由伺服器設定，不會洩露給用戶端。
+    resave: false, //當設定為 true 時，會在每一次與使用者互動後，強制把 session 更新到 session store 裡。
+    saveUninitialized: true, //強制將未初始化的 session 存回 session store。未初始化表示這個 session 是新的而且沒有被修改過，例如未登入的使用者的 session。
+  })
+);
+
+// setting body-parser
+app.use(express.urlencoded({ extended: true }));
+
+// 設定每一筆請求都會透過 methodOverride 進行前置處理
+app.use(methodOverride("_method"));
+
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app);
+
+// 掛載套件
+app.use(flash());
+
+//使用 app.use 代表這組 middleware 會作用於所有的路由
+app.use((req, res, next) => {
+  // console.log(req.user); //在反序列化的時候，取出的 user 資訊
+  // {
+  // _id: 6360d8d82f2cdb12c0beb392,
+  // name: 'lin',
+  // email: 'wa@gmail.com',
+  // password: 'WmRL.4BXUjbAN',
+  // createdAt: 2022-11-01T08:29:12.292Z,
+  // __v: 0
+  // }
+  res.locals.isAuthenticated = req.isAuthenticated(); //把 req.isAuthenticated() 回傳的布林值，交接給 res 使用
+  res.locals.user = req.user; //把使用者資料交接給 res 使用
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.warning_msg = req.flash("warning_msg");
+  next(); //res.locals：所有樣板都可以使用的變數，放在 res.locals 裡的資料，所有的 view 都可以存取。
+});
 
 // 將 request 導入路由器
 app.use(routes);
---
+++ config/passport.js
@@ -0,0 +1,126 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy; //passport-local的Strategy物件
+const FacebookStrategy = require("passport-facebook").Strategy;
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const user = require("../models/user");
+
+module.exports = (app) => {
+  // 初始化 Passport 模組
+  app.use(passport.initialize());
+  app.use(passport.session());
+  // 設定本地登入策略
+  passport.use(
+    new LocalStrategy(
+      { usernameField: "email", passReqToCallback: true },
+      (req, email, password, done) => {
+        //把驗證項目從預設的 username 改成 email。
+        //欄位名稱email是字串所以要加引號
+        // done( 錯誤訊息/是否有錯(null or err), 使用者資訊(truthy or false), 訊息 )
+        // A verify function yields under one of three conditions: success, failure, or an error.
+        User.findOne({ email })
+          .then((user) => {
+            if (!user) {
+              return done(
+                null,
+                false,
+                req.flash("warning_msg", "That email is not registered!")
+              ); //If the credential does not belong to a known user, or is not valid, the verify function calls the callback with false to indicate an authentication failure。 找不到使用者輸入的 username，用戶物件為 false
+            }
+            //第一個參數是使用者的輸入值，而第二個參數是資料庫裡的雜湊值，bcrypt 會幫我們做比對，並回傳布林值 (我設為isMatch)
+            return bcrypt.compare(password, user.password).then((isMatch) => {
+              if (!isMatch) {
+                //原本是 user.password !== password
+                return done(
+                  null,
+                  false,
+                  req.flash("warning_msg", "Email or Password incorrect.")
+                ); //找到使用者，但發現密碼錯誤，用戶物件為 false
+              }
+              return done(null, user); //success
+            });
+          })
+          //If an error occurs, such as the database not being available, the callback is called with an error
+          .catch((err) => done(err, false)); //其實 done(err) 就夠了
+      }
+    )
+  );
+
+  // 設定facebook策略
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FACEBOOK_ID,
+        clientSecret: process.env.FACEBOOK_SECRET,
+        callbackURL: process.env.FACEBOOK_CALLBACK,
+        profileFields: ["email", "displayName"],
+      },
+      (accessToken, refreshToken, profile, done) => {
+        console.log("profile", profile);
+        // profile {
+        //   id: '5468445146599841',
+        //   username: undefined,
+        //   displayName: '林奕聞',
+        //   name: {
+        //     familyName: undefined,
+        //     givenName: undefined,
+        //     middleName: undefined
+        //   },
+        //   gender: undefined,
+        //   profileUrl: undefined,
+        //   emails: [ { value: 'wayne403z@yahoo.com.tw' } ],
+        //   provider: 'facebook',
+        //   _raw: '{"email":"wayne403z\\u0040yahoo.com.tw","name":"\\u6797\\u5955\\u805e","id":"5468445146599841"}',
+        //   _json: {
+        //     email: 'wayne403z@yahoo.com.tw',
+        //     name: '林奕聞',
+        //     id: '5468445146599841'
+        //   }
+        // }
+        console.log("accessToken", accessToken);
+        // EAAJdBzryUF4BADJbCaQY2wznOH8ud8qpwlIzZC7K7VSi7qUZAN87WnPbJZAIfZCCu2qmN0Fsk1zxdOnbKOXpb7ZAEGZCXlAnLq2hZCvEKKqTIbPfXq9PoKlxLG13NO2v0CXloCAnVb3wxjqfTG6OUmIT26i4VIxK8uhQZBZAzrDabShZA7hcjOHwbrplHZArIwPmFydscvjNdaYJiaBF30t088AzPLW6ZBbZABV3H2YIHMN3v0gZDZD
+        console.log("refreshToken", refreshToken);
+        //undefined
+        console.log("done", done);
+        //[Function: verified]
+
+        const { name, email } = profile._json;
+        User.findOne({ email }).then((user) => {
+          if (user) return done(null, user);
+          const randomPassword = Math.random().toString(36).slice(-8);
+          bcrypt
+            .genSalt(10)
+            .then((salt) => bcrypt.hash(randomPassword, salt))
+            .then((hash) =>
+              User.create({
+                name,
+                email,
+                password: bcrypt.hash,
+              })
+            )
+            .then((user) => done(null, user))
+            .catch((err) => done(err, false));
+        });
+      }
+    )
+  );
+  // 設定序列化與反序列化
+  passport.serializeUser((user, done) => {
+    // console.log(user);
+    // {
+    //   _id: 6360d8d82f2cdb12c0beb392,
+    //   name: 'lin',
+    //   email: 'wa@gmail.com',
+    //   password: 'WmRL.4BXUjbAN',
+    //   createdAt: 2022-11-01T08:29:12.292Z,
+    //   __v: 0
+    // }
+    done(null, user.id); //mongoose幫忙做出的_id，這裡用別名(不用底線)id也可
+  });
+  passport.deserializeUser((id, done) => {
+    User.findById(id)
+      .lean() //把資料庫物件轉換成 JavaScript 原生物件
+      .then((user) => done(null, user))
+      .catch((err) => done(err, null)); //其實Passport看到第一個參數有 err 就不會處理後面的參數了，放一個 null 只是語意表達用
+  });
+};
